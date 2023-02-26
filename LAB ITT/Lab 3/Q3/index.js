9cd9e063e4455a039f64836645ba7ff2af02a890
glytch3 <92800167+glytch3@users.noreply.github.com>
2023-02-27 00:35:56 +0530

    Delete LAB ITT directory

--- LAB ITT/Lab 3/Q3/index.js
+++
@@ -1,9 +0,0 @@
-function find(){
-    let entered = document.getElementById("entry").value;
-    let areaval = entered.substr(1,3)
-    let phoneval = entered.substr(5,12)
-    let area = document.getElementById("area")
-    let phone = document.getElementById("phone")
-    area.textContent = areaval;
-    phone.textContent = phoneval;
-}
--- LAB ITT/Lab 3/Q5/index.js
+++
@@ -1,9 +0,0 @@
-function find(){
-    let entered = document.getElementById("entry").value;
-    document.write("<table> <tr><td>Number</td><td>Square</td><td>Cube</td></tr>")
-   for(let i=0;i<=entered;i++)
-    {
-        document.write("<tr><td>"+i+"</td><td>"+(i*i)+"</td><td>"+(i*i*i)+"</td></tr>")
-    }
-    document.write("</table>")
-}
